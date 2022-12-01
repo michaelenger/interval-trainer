3e24523d0dd6d03f1df0f5edd7cdbf7053f48369
Michael Enger <git@michaelenger.com>
2022-12-01 22:22:17 +0000

    Change notes to not be hardcoded

--- script.js
+++ script.js
@@ -2,6 +2,24 @@
 
 (function() {
 
-console.log("Ready to go!");
+const LETTERS = [
+	"A", "B", "C", "D", "E", "F", "G"
+];
+
+const tonicElement = document.getElementById("tonic");
+const unknownElement = document.getElementById("unknown");
+
+function makeGuess() {
+	// TODO: Make random
+	const tonic = 2;
+	const unknown = 5;
+	const isTonicFlat = false;
+	const isUnknownFlat = true;
+
+	tonicElement.innerHTML = LETTERS[tonic] + (isTonicFlat ? "&flat;" : "");
+	unknownElement.innerHTML = LETTERS[unknown] + (isUnknownFlat ? "&flat;" : "");
+}
+
+makeGuess();
 
 })();
