300049bf7a05b56fb6c73c797f21aeb3dd8610a4
Michael Enger <git@michaelenger.com>
2022-12-01 23:25:53 +0000

    Add mechanism for checking your guess

--- script.js
+++ script.js
@@ -2,34 +2,113 @@
 
 (function () {
 
-const LETTERS = [
+const NOTES = [
 	'A', 'B', 'C', 'D', 'E', 'F', 'G'
 ];
+const NOTE_DISTANCES = [
+	2, 1, 2, 2, 1, 2, 2
+];
+const STEP_DISTANCES = {
+	'dim2': 0,
+	'min2': 1,
+	'maj2': 2,
+	'aug2': 3,
+
+	'dim3': 2,
+	'min3': 3,
+	'maj3': 4,
+	'aug3': 5,
+
+	'dim4': 4,
+	'perf4': 5,
+	'aug4': 6,
+
+	'dim5': 6,
+	'perf5': 7,
+	'aug5': 8,
+
+	'dim6': 7,
+	'min6': 8,
+	'maj6': 9,
+	'aug6': 10,
+
+	'dim7': 9,
+	'min7': 10,
+	'maj7': 11,
+	'aug7': 12,
+};
 
 const guessStepElement = document.getElementById('guess-step');
 const guessVariationElement = document.getElementById('guess-variation');
 const tonicElement = document.getElementById('tonic');
 const unknownElement = document.getElementById('unknown');
 
+var tonic = 0;
+var unknown = 0;
+var isTonicFlat = false;
+var isUnknownFlat = true;
+
 function createChallenge() {
 	// TODO: Make random
-	const tonic = 2;
-	const unknown = 5;
-	const isTonicFlat = false;
-	const isUnknownFlat = true;
+	tonic = 2;
+	unknown = 1;
+	isTonicFlat = true;
+	isUnknownFlat = false;
 
-	tonicElement.innerHTML = LETTERS[tonic] + (isTonicFlat ? '&flat;' : '');
-	unknownElement.innerHTML = LETTERS[unknown] + (isUnknownFlat ? '&flat;' : '');
+	tonicElement.innerHTML = NOTES[tonic] + (isTonicFlat ? '&flat;' : '');
+	unknownElement.innerHTML = NOTES[unknown] + (isUnknownFlat ? '&flat;' : '');
 }
 
 function checkGuess() {
 	const step = guessStepElement.value;
 	const variation = guessVariationElement.value;
 
-	console.log(`CHECK: ${variation} ${step}`);
+	if (step === '' || variation == '')
+		return; // assume it was a mistake
 
-	guessStepElement.value = '';
-	guessVariationElement.value = '';
+	const expectedStep = unknown - tonic + 1 + (unknown < tonic ? NOTES.length : 0);
+
+	const expectedDistance = determineNoteDistance(tonic, isTonicFlat, unknown, isUnknownFlat);
+	const distance = determineStepDistance(step, variation);
+
+	console.log(`CHECK: ${step} vs ${expectedStep}`);
+	console.log(`CHECK: ${distance} vs ${expectedDistance}`);
+	if (step != expectedStep || distance != expectedDistance) {
+		// TODO: Show failure
+		console.warn('Incorrect');
+	} else {
+		// TODO: Show success
+		console.log('Correct!');
+	}
+}
+
+function determineNoteDistance(first, isFirstFlat, second, isSecondFlat) {
+	var distance = 0;
+
+	if (isFirstFlat)
+		distance += 1;
+
+	while (first != second) {
+		distance += NOTE_DISTANCES[first];
+		first += 1;
+		if (first === NOTES.length) {
+			first = 0;
+		}
+	}
+
+	if (isSecondFlat)
+		distance -= 1;
+
+	return distance;
+}
+
+function determineStepDistance(step, variation) {
+	const key = `${variation}${step}`;
+
+	if (key in STEP_DISTANCES)
+		return STEP_DISTANCES[key];
+
+	return -1;
 }
 
 document.getElementById('guess').addEventListener('submit', (e) => {
