6cc7769507cb13e5f632ef37310ba4a3153a1588
Michael Enger <git@michaelenger.com>
2022-12-01 22:30:57 +0000

    Intercept form submit to check the guess

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ const LETTERS = [
 	"A", "B", "C", "D", "E", "F", "G"
 ];
 
+const guessStepElement = document.getElementById("guess-step");
+const guessVariationElement = document.getElementById("guess-variation");
 const tonicElement = document.getElementById("tonic");
 const unknownElement = document.getElementById("unknown");
 
-function makeGuess() {
+function createChallenge() {
 	// TODO: Make random
 	const tonic = 2;
 	const unknown = 5;
@@ -20,6 +22,21 @@ function makeGuess() {
 	unknownElement.innerHTML = LETTERS[unknown] + (isUnknownFlat ? "&flat;" : "");
 }
 
-makeGuess();
+function checkGuess() {
+	const step = guessStepElement.value;
+	const variation = guessVariationElement.value;
+
+	console.log(`CHECK: ${variation} ${step}`);
+
+	guessStepElement.value = "";
+	guessVariationElement.value = "";
+}
+
+document.getElementById("guess").addEventListener("submit", (e) => {
+	e.preventDefault();
+	checkGuess();
+})
+
+createChallenge();
 
 })();
